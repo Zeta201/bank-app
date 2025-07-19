9d477fab27fd358c9e1626493d4c9ef855ee71ec
Dinitha Wijewardhana <dini2040gh@gmail.com>
2025-07-19 12:55:02 +0530

    Fix config.js script tag

--- public/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-const config = {
-  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'https://default-api-url.com', // Fallback URL
-};
-
-export default config;
--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,22 @@
 // src/services/api.js
 
 import axios from 'axios';
-import config from '../../public/config';
 
-const API_BASE_URL = config.apiBaseUrl; // Get base URL from config
+const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
+
 // Users
-export const createUser = (userData) => axios.post(`${API_BASE_URL}/users`, userData);
-export const getUserById = (userId) => axios.get(`${API_BASE_URL}/users/${userId}`);
+export const createUser = (userData) => axios.post(`${apiUrl}/users`, userData);
+export const getUserById = (userId) => axios.get(`${apiUrl}/users/${userId}`);
 
 // Accounts
-export const createAccount = (accountData) => axios.post(`${API_BASE_URL}/accounts`, accountData);
-export const deleteAccount = (accountNo) => axios.delete(`${API_BASE_URL}/accounts/${accountNo}`);
-export const depositToAccount = (accountNo, amount) => axios.post(`${API_BASE_URL}/accounts/${accountNo}/deposit`, { amount });
-export const withdrawFromAccount = (accountNo, amount) => axios.post(`${API_BASE_URL}/accounts/${accountNo}/withdraw`, { amount });
-export const transferFunds = (fromAccount, toAccount, amount) => axios.post(`${API_BASE_URL}/accounts/transfer/${fromAccount}/${toAccount}`, { amount });
+export const createAccount = (accountData) => axios.post(`${apiUrl}/accounts`, accountData);
+export const deleteAccount = (accountNo) => axios.delete(`${apiUrl}/accounts/${accountNo}`);
+export const depositToAccount = (accountNo, amount) => axios.post(`${apiUrl}/accounts/${accountNo}/deposit`, { amount });
+export const withdrawFromAccount = (accountNo, amount) => axios.post(`${apiUrl}/accounts/${accountNo}/withdraw`, { amount });
+export const transferFunds = (fromAccount, toAccount, amount) => axios.post(`${apiUrl}/accounts/transfer/${fromAccount}/${toAccount}`, { amount });
 
 // Transactions
-export const getAllTransactions = () => axios.get(`${API_BASE_URL}/transactions`);
-export const getTransactionById = (transactionId) => axios.get(`${API_BASE_URL}/transactions/${transactionId}`);
-export const getTransactionsByUserId = (userId) => axios.get(`${API_BASE_URL}/users/${userId}/transactions`);
-export const getTransactionsByAccountNo = (accountNo) => axios.get(`${API_BASE_URL}/accounts/${accountNo}/transactions`);
+export const getAllTransactions = () => axios.get(`${apiUrl}/transactions`);
+export const getTransactionById = (transactionId) => axios.get(`${apiUrl}/transactions/${transactionId}`);
+export const getTransactionsByUserId = (userId) => axios.get(`${apiUrl}/users/${userId}/transactions`);
+export const getTransactionsByAccountNo = (accountNo) => axios.get(`${apiUrl}/accounts/${accountNo}/transactions`);
